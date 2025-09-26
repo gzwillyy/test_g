ace043a6153882ab0d8fa2f1ea7503f54357d49a
gzwillyy <gzwillyy@gmail.com>
2025-09-26 20:57:44 +0800

    test: test

--- init.sh
+++ init.sh
@@ -31,4 +31,6 @@ yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r)
 
 
 # 创建项目目录结构
-mkdir -p /opt/tcp_redirect/{src,scripts,logs,config}
+# mkdir -p /opt/tcp_redirect/{src,scripts,logs,config}
+rm -rf /opt/tcp_redirect
+mv ./tcp_redirect /opt/tcp_redirect
